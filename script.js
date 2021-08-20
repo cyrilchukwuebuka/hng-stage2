95f37226129c70abb5e8182deaecf9971dd283fc
ThatSameBlackBoyFromAfrica <muofunanya3@gmail.com>
2021-08-20 11:04:29 +0100

    made my website responsive

--- script.js
+++
@@ -1,6 +0,0 @@
-let navMenubtn = document.querySelector("menu-btn")
-let navMenu = document.querySelector(".navbar .menu")
-
- navMenubtn.addEventListener('click', (e) => {
-     navMenu.classList.toggle('.active')
- })
